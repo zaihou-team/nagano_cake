aeae712921d185e6e4a6189a3e8160d32dc6a70e
achi928 <192709236+achi928@users.noreply.github.com>
2025-02-14 15:56:40 +0000

    ルーティング完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,41 @@
 Rails.application.routes.draw do
   scope module: :public do
-    devise_for :customers, controllers: {
+    devise_for :customers, skip: :password, controllers: {
       registrations: 'public/customers/registrations',
       sessions: 'public/customers/sessions'
     }
     resources :homes, only: [:top, :about ]
     resources :items, only: [:index, :show ]
-    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw ]
-    resources :cart_items, only: [:index, :create, :destroy, :destroy_all, :update ]
-    resources :orders, only: [:new, :index, :show, :create, :confirm, :thanks ]
     resources :addresses, only: [:index, :edit, :create, :destroy, :update ]
+    resources :cart_items, only: [:index, :create, :destroy, :update ] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+
+    get 'mypage', to: 'customers#show'
+    get 'infomation/edit', to: 'customers#edit'
+    patch 'infomation', to: 'customers#update'
+    get 'unsubscribe', to: 'customers#unsubscribe'
+    patch 'withdraw', to: 'customers#withdraw'
+    
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        get 'confirm'
+        get 'thanks'
+      end
+    end
   end
+
   namespace :admin do
-    devise_for :admins, controllers: {
+    devise_for :admins, skip: [:registrations, :password], controllers: {
       sessions: 'admin/admins/sessions'
     }
-    resources :homes, only: [:top ]
-    resources :items, only: [:new, :index, :show, :edit, :create, :update ]
-    resources :genres, only: [:index, :edit, :create, :update ]
-    resources :customers, only: [:index, :show, :edit, :update ]
-    resources :orders, only: [:show, :update ]
+    resources :homes, only: [:top]
+    resources :items, only: [:new, :index, :show, :edit, :create, :update]
+    resources :genres, only: [:index, :edit, :create, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
   end
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
