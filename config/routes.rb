c95abe490cba2722fcf3bf5c571893adb92c39a0
achi928 <192709236+achi928@users.noreply.github.com>
2025-02-14 10:12:14 +0000

    view削除後

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/top'
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
