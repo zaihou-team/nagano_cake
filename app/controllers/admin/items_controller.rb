2d3679ad2f49f7c3023e28f3ed2049292bd1d447
achi928 <192709236+achi928@users.noreply.github.com>
2025-02-17 18:43:24 +0000

    ジャンルOK、商品の一覧、詳細OK

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,27 @@
 class Admin::ItemsController < ApplicationController
+
+    def index
+      @items = Item.all.page(params[:page])
+    end
+  
+    def show
+      @item = Item.find(params[:id])
+    end
+  
+    def edit
+      @item = Item.find(params[:id])
+    end
+  
+    def update
+      @item = Item.find(params[:id])
+      @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    end
+  
+    private
+  
+    def item_params
+      params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active)
+    end
+  
 end
