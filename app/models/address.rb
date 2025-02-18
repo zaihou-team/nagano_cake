385702a19bb288235a2b290944c809379e04ba4b
EC2 Default User <ec2-user@ip-172-31-27-253.ap-northeast-1.compute.internal>
2025-02-18 01:58:09 +0000

    配送先機能を実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
+
+  validates :postal_code, presence: true, length: { is: 7 }
+  validates :address, presence: true
+  validates :name, presence: true
 end
