a69f73dc4166ab4f3d6ffdceade877c9b602bf4c
EC2 Default User <ec2-user@ip-172-31-4-147.ap-northeast-1.compute.internal>
2025-02-20 05:20:27 +0000

    商品一覧、詳細

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/delicious_cake.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+      image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
