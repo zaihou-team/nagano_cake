61b6cb1df258758231adfc569dc6997c59fbb9a8
achi928 <192709236+achi928@users.noreply.github.com>
2025-02-21 05:17:36 +0000

    controllerの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,5 @@
 class ApplicationController < ActionController::Base
     helper_method :current_customer
-    
-    
-  protected
 
-  def after_sign_in_path_for(resource)
-    case resource
-    when Admin
-      admin_root_path 
-    when Customer
-      root_path 
-    else
-      root_path
-    end
-  end
 
 end
