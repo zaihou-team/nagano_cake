9026f9ed409880d5a0669954ee409fd39f64974d
EC2 Default User <ec2-user@ip-172-31-4-147.ap-northeast-1.compute.internal>
2025-02-23 06:38:02 +0000

    名前表記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_customer
+    helper_method :current_customer, :admin_signed_in?
+    before_action :hide_customer_name_for_admin
 
 
+    private
+
+    def hide_customer_name_for_admin
+        @hide_customer_name = request.path.start_with?('/admin')
+    end
 end
